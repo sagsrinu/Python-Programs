a03369a2c3d33e9d76c85e8023a481386729eff9
Srinivasarao <sagsrinu@gmail.com>
2026-04-18 07:03:09 +0530

    Add files via upload

---
+++ data type conversion.py
@@ -0,0 +1,37 @@
+###Converting numbers from octal,binary and hexa decimal to number system
+
+x=15.56
+int(x)
+
+# Output is - 15
+
+num =25
+float(num)
+
+#Output is - 25.0
+
+a=10
+b=20
+complex(a,b)
+
+#Output is : 10+20j
+
+
+
+n1=0O17
+n2=0B1110010
+n3=0X1c2
+
+n=int(n1)
+print('Octal  = ',n)
+
+# Output is - 15
+
+n=int(n2)
+print('Binary =',n)
+
+# Output is - 114
+
+n=int(n3)
+print('Hexadecimal =',n)
+# Outout is - 450
